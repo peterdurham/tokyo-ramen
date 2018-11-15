60d6bbbc07adb5e3f1f7a1be4d5f43c95e1a0aba
Peter Durham <peterdurham@protonmail.com>
2018-11-15 08:55:35 -0800

    Updates

---
+++ precache-manifest.85d03220908a0d9b8babda1b3b8e2942.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "b24aa117619295beaa4aa38884c9c17e",
+    "url": "/tokyo-ramen/static/media/japanrestaurant.b24aa117.jpg"
+  },
+  {
+    "revision": "a5184d306b066dee7039",
+    "url": "/tokyo-ramen/static/css/main.02aee53f.chunk.css"
+  },
+  {
+    "revision": "779e5bf5ab70ab74e0de",
+    "url": "/tokyo-ramen/static/js/1.779e5bf5.chunk.js"
+  },
+  {
+    "revision": "cca8ad69203b9035b735",
+    "url": "/tokyo-ramen/static/js/runtime~main.cca8ad69.js"
+  },
+  {
+    "revision": "e1a63069663f85cd44a365664e91e6fc",
+    "url": "/tokyo-ramen/static/media/drinks.e1a63069.jpg"
+  },
+  {
+    "revision": "5cd74ed0d67695814f916edba9905a25",
+    "url": "/tokyo-ramen/static/media/dessert.5cd74ed0.jpg"
+  },
+  {
+    "revision": "89ffa90af9d453fb0a14db93a46ac9d4",
+    "url": "/tokyo-ramen/static/media/appetizers.89ffa90a.jpg"
+  },
+  {
+    "revision": "06b0be7a55a2970a8edcecfdf84dfe86",
+    "url": "/tokyo-ramen/static/media/japanesetext.06b0be7a.jpg"
+  },
+  {
+    "revision": "2e43111267a56de3774152d31f6dd621",
+    "url": "/tokyo-ramen/static/media/ramenkitchen.2e431112.jpg"
+  },
+  {
+    "revision": "050ce13434fd62c66bee634dee6feaa8",
+    "url": "/tokyo-ramen/static/media/japannighttime.050ce134.jpg"
+  },
+  {
+    "revision": "9aacc96a95ba2123893feb05f5d98318",
+    "url": "/tokyo-ramen/static/media/ramenbowl3.9aacc96a.jpg"
+  },
+  {
+    "revision": "7e6a36720a87f133a632907ba2d399d5",
+    "url": "/tokyo-ramen/static/media/tokyodowntown.7e6a3672.jpg"
+  },
+  {
+    "revision": "f16a99f8b5858af2c1da500d44d4b00c",
+    "url": "/tokyo-ramen/static/media/japanwall.f16a99f8.jpg"
+  },
+  {
+    "revision": "c5dbefb25f8ed4efe1f97aea463b6833",
+    "url": "/tokyo-ramen/static/media/meal.c5dbefb2.jpg"
+  },
+  {
+    "revision": "a5184d306b066dee7039",
+    "url": "/tokyo-ramen/static/js/main.a5184d30.chunk.js"
+  },
+  {
+    "revision": "7308c2965ffc7625c19f7690fde3df32",
+    "url": "/tokyo-ramen/static/media/japanalley2.7308c296.jpg"
+  },
+  {
+    "revision": "8778901abd73d1cba249e5eb81db6e9e",
+    "url": "/tokyo-ramen/static/media/tokyo.8778901a.jpg"
+  },
+  {
+    "revision": "4d1548d8ddaf3189487a8d928909331f",
+    "url": "/tokyo-ramen/static/media/food.4d1548d8.jpg"
+  },
+  {
+    "revision": "b82fd9e16fbec86e1548ee7b6c085600",
+    "url": "/tokyo-ramen/static/media/gallery1.b82fd9e1.jpg"
+  },
+  {
+    "revision": "f8a6bae0f3c3cc2d474d298d64cd099d",
+    "url": "/tokyo-ramen/static/media/gallery2.f8a6bae0.jpg"
+  },
+  {
+    "revision": "017e304448de80f39dbbc365e1c52a4d",
+    "url": "/tokyo-ramen/static/media/gallery3.017e3044.jpg"
+  },
+  {
+    "revision": "562ac6cacf72fa9c9ffb8373a7bb989f",
+    "url": "/tokyo-ramen/static/media/gallery4.562ac6ca.jpg"
+  },
+  {
+    "revision": "2c7dc2b7d097efb2649802bd14be025e",
+    "url": "/tokyo-ramen/static/media/gallery5.2c7dc2b7.jpg"
+  },
+  {
+    "revision": "7563df77d83c0c8aa62b8d20e47fd5be",
+    "url": "/tokyo-ramen/static/media/gallery6.7563df77.jpg"
+  },
+  {
+    "revision": "b4eb8750d8bc205dd310f3506b1eeb36",
+    "url": "/tokyo-ramen/static/media/gallery7.b4eb8750.jpg"
+  },
+  {
+    "revision": "84cfd6bc9facbf90d010e74dc405daa9",
+    "url": "/tokyo-ramen/static/media/gallery8.84cfd6bc.jpg"
+  },
+  {
+    "revision": "efb2d529831a5ecfe943803d4e5fcbc2",
+    "url": "/tokyo-ramen/static/media/tokyo1.efb2d529.jpg"
+  },
+  {
+    "revision": "c56940143ebb44850070b177b3f26617",
+    "url": "/tokyo-ramen/static/media/tokyo2.c5694014.jpg"
+  },
+  {
+    "revision": "e892bfdc22617b48f20b925260297e4b",
+    "url": "/tokyo-ramen/index.html"
+  }
+];
